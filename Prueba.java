b3f5edd1418d993bda42e41e36d23d1046c76d50
Diego Martinez <diegojmartinezz@hotmail.com>
2016-04-11 16:44:06 -0500

    Un cambio

--- Prueba.java
+++ Prueba.java
@@ -2,6 +2,6 @@ public class Prueba{
 	public static void main(String args[]){
 		System.out.println("Esto nunca va a correr");
 		System.out.println("Solo existe para jugar");
-		System.out.println("Esto deberia estar master y Rama");
+		System.out.println("Commit pre merge");
 	}
 }		
